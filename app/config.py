22a692dc37e7e383437a39cc56838ea6649d1d31
tahsin242 <tahsin.munawar1@gmail.com>
2026-09-12 22:32:00 +0600

    pay:

--- app/config.py
+++ app/config.py
@@ -9,6 +9,8 @@ class Settings(BaseSettings):
     secret_key : str
     algorithm: str
     access_token_expire_minutes: int
+    stripe_secret_key: str
+    stripe_webhook_secret:str
 
     class Config:
         env_file = ".env"
--- app/models.py
+++ app/models.py
@@ -22,7 +22,7 @@ class User(Base):
     email =  Column(String, nullable = False, unique = True)
     password =  Column(String, nullable = False)
     created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = text('now()'))
-
+    is_premium = Column(Boolean, nullable=False, default=False)
 
 class Vote(Base):
     __tablename__ = "votes"
